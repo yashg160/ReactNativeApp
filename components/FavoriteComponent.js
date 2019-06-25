35aa75f2243d628cad59181df8827bfa0dfb9123
Yash Gupta <yashg160@gmail.com>
2019-06-25 16:51:23 +0530

    Added FavoritesComponent

---
+++ components/FavoriteComponent.js
@@ -0,0 +1,61 @@
+import { ListItem } from 'react-native-elements';
+import { FlatList } from 'react-native';
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
+
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes,
+        favorites: state.favorites
+    }
+}
+
+class Favorites extends Component{
+
+    static navigationOptions = {
+        title: 'My Favorites'
+    }
+
+    render() {
+        const { navigate } = this.props.navigation;
+
+        const renderMenuItem = ({ item, index }) => {
+            return (
+                <ListItem
+                    key={index}
+                    titel={item.name}
+                    subtitle={item.description}
+                    hideChevron={true}
+                    onPress={() => navigate('Dishdetail', { dishId: item.id })}
+                    leftAvatar={{ source: {uri: baseUrl + item.image}}}
+                />
+            );
+        }
+
+        if (this.props.dishes.isLoading) {
+            return (
+                <Loading />
+            )
+        }
+        else if (this.props.dishes.errMess) {
+            return (
+                <View>
+                    <Text>{this.props.dishes.errMess}</Text>
+                </View>
+            )
+        }
+        else {
+            return (
+                <FlatList
+                    data={this.props.dishes.dishes.filter(dish => this.props.favorites.some(el => el === dish.id))}
+                    renderItem={renderMenuItem}
+                    keyExtractor={item => item.id.toString()}
+                />
+            )
+        }
+    }
+}
+
+export default connect(mapStateToProps)(Favorites)
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,6 +5,7 @@ import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoriteComponent';
 import { View, Platform, Image, StyleSheet, ScrollView, Text } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
@@ -111,6 +112,22 @@ const ReservationNavigator = createStackNavigator({
 
         })
     });
+
+const FavoritesNavigator = createStackNavigator({
+    About: { screen: Favorites }
+}, {
+        navigationOptions: ({ navigation }) => ({
+            headerStyle: {
+                backgroundColor: '#512DA8'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon name='menu' size={24} color='white' onPress={() => navigation.toggleDrawer()} />
+
+        })
+    });
 const CustomDrawerContentComponent = (props) => (
     <ScrollView>
         <SafeAreaView style={styles.container}
@@ -199,6 +216,21 @@ const MainNavigator = createDrawerNavigator({
             drawerIcon: ({ tintColor }) => (
                 <Icon
                     name='cutlery'
+                    type='font-awesome'
+                    size={24}
+                    color={tintColor}
+                />
+            )
+        }
+    },
+    Favorites: {
+        screen: FavoritesNavigator,
+        navigationOptions: {
+            title: 'My Favorites',
+            drawLabel: 'My Favorites',
+            drawerIcon: ({ tintColor }) => (
+                <Icon
+                    name='heart'
                     type='font-awesome'
                     size={24}
                     color={tintColor}
