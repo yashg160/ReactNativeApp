6b948fe09b5cc9e45b852440a951a85bd4bd2776
Yash Gupta <yashg160@gmail.com>
2019-06-27 19:14:34 +0530

    Image Manipulator

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -1,9 +1,11 @@
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import React, { Component } from 'react';
-import { Card, Icon, Input, CheckBox } from 'react-native-elements';
-import { SecureStore } from 'expo';
+import { Icon, Input, CheckBox, Button } from 'react-native-elements';
+import { SecureStore, Permissions, ImagePicker, ImageManipulator, Asset } from 'expo';
+import { createBottomTabNavigator } from 'react-navigation';
+import { baseUrl } from '../shared/baseUrl';
 
-class Login extends Component {
+class LoginTab extends Component {
     
     constructor(props) {
         super(props);
@@ -27,7 +29,15 @@ class Login extends Component {
     }
 
     static navigationOptions = {
-        title: 'Login'
+        title: 'Login',
+        tabBarIcon: ({ tintColor }) => (
+            <Icon
+                name='sign-in'
+                type='font-awesom'
+                size={24}
+                iconStyle={{color: tintColor}}
+            />
+        )
     }
 
     handleLogin() {
@@ -73,7 +83,18 @@ class Login extends Component {
                     <Button
                         onPress={() => this.handleLogin()}
                         title="Login"
-                        color='#512DA8'
+                        icon={<Icon name='sign-in' type='font-awesome' color='white' />}
+                        buttonStyle={{ backgroundColor:'#512DA8' }}
+                    />
+                </View>
+
+                <View style={styles.formButton}>
+                    <Button
+                        onPress={() => this.props.navgation.navigate('Register')}
+                        title="Register"
+                        clear
+                        icon={<Icon name='user-plus' type='font-awesome' color='blue' />}
+                        titleStyle={{color: 'blue'}}
                     />
                 </View>
             </View>
@@ -83,9 +104,172 @@ class Login extends Component {
     
 }
 
+class RegisterTab extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            firstname: '',
+            lastname: '',
+            email: '',
+            remember: false,
+            imageUrl: baseUrl + 'images/logo.png'
+        }
+    }
+
+    handleRegister() {
+        console.log(JSON.stringify(this.state));
+        if (this.state.remember) {
+            SecureStore.setItemAsync(
+                'userinfo',
+                JSON.stringify({ username: this.state.username, password: this.state.password })
+            )
+                .catch(error => console.log('Could not save user info', error))
+        }
+    }
+
+    render() {
+        return (
+            <ScrollView>
+                <View style={styles.container}>
+                    <View style={styles.imageContainer}>
+                        <Image
+                            source={{ uri: this.state.imageUrl }}
+                            loadingIndicatorSource={require('./images/logo.png')}
+                            style={styles.image}
+                        />
+                        <Button
+                            title='Camera'
+                            onPress={()=> this.getImageFromCamera()}
+                        />
+                    </View>
+                    <Input
+                        placeholder='Username'
+                        leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                        onChangeText={username => this.setState({ username })}
+                        value={this.state.username}
+                        containerStyle={styles.formInput}
+                    />
+                    <Input
+                        placeholder='Password'
+                        leftIcon={{ type: 'font-awesome', name: 'key' }}
+                        onChangeText={password => this.setState({ password })}
+                        value={this.state.password}
+                        containerStyle={styles.formInput}
+                    />
+                    <Input
+                        placeholder='Firstname'
+                        leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                        onChangeText={firstname => this.setState({ firstname })}
+                        value={this.state.firstname}
+                        containerStyle={styles.formInput}
+                    />
+                    <Input
+                        placeholder='Lastname'
+                        leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                        onChangeText={lastname => this.setState({ lastname })}
+                        value={this.state.lastname}
+                        containerStyle={styles.formInput}
+                    />
+                    <Input
+                        placeholder='Email'
+                        leftIcon={{ type: 'font-awesome', name: 'envelope-o' }}
+                        onChangeText={email => this.setState({ email })}
+                        value={this.state.email}
+                        containerStyle={styles.formInput}
+                    />
+                    <CheckBox
+                        title='Remember Me'
+                        center
+                        checked={this.state.remember}
+                        onPress={() => this.setState({ remember: !this.state.remember })}
+                        style={styles.formCheckbox}
+                    />
+                    <View style={styles.formButton}>
+                        <Button
+                            onPress={() => this.handleRegister()}
+                            title="Register"
+                            icon={<Icon name='user-plus' type='font-awesome' color='white' />}
+                            buttonStyle={{backgroundColor:'#512DA8'}}
+                        />
+                    </View>
+                </View>
+            </ScrollView>
+        );
+    }
+    
+    getImageFromCamera = async() => {
+    const cameraPermission = await Permissions.askAsync(Permissions.CAMERA);
+    const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL); 
+
+        if (cameraPermission.status === 'granted' && cameraRollPermission.status === 'granted') {
+            console.log("Camera Permission granted");
+            let capturedImage = await ImagePicker.launchCameraAsync({
+                allowsEditing: true,
+                aspect: [4, 3],
+            });
+
+            if (!capturedImage.cancelled) {
+                this.processImage(capturedImage.uri);
+            }
+            else {
+                console.log('Captured Image discarded');
+            }
+        }
+    }
+
+    processImage = async (ImageUri) => {
+        let processedImage = await ImageManipulator.manipulate(
+            ImageUri,
+            [
+                { resize: { width: 400 } }
+            ],
+            { format: 'png' }
+        );
+        console.log(JSON.stringify(processedImage));
+        this.setState({ ImageUrl: processedImage.uri });
+    }
+
+    static navigationOptions = {
+        title: 'Register',
+        tabBarIcon: ({ tintColor }) => (
+            <Icon
+                name='user-plus'
+                type='font-awesome'
+                size={24}
+                iconStyle={{ color: tintColor }}
+            />
+        )
+    }
+}
+
+const Login = createBottomTabNavigator({
+    Login: LoginTab,
+    Register: RegisterTab
+}, {
+        tabBarOptions: {
+            activeBackgroundColor: '#9575CD',
+            inactiveBackgroundColor: '#D1C4E9',
+            activeTintColor: '#ffffff',
+            inactiveTintColor: 'gray'
+            
+        }
+    });
+
 const styles = StyleSheet.create({
     container: {
         justifyContent: 'center'
+    },
+    imageContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    image: {
+        margin: 10,
+        width: 80,
+        height: 60
     },
     formInput: {
         margin: 40
